941cf246acb508d2fce1dff4b6e2a496aff7203d
SWAPNILXYZ <66053332+SWAPNILXYZ@users.noreply.github.com>
2020-08-23 12:12:01 +0530

    Add files via upload

---
+++ dustbin.js
@@ -0,0 +1,25 @@
+   class Dustbin{
+    constructor(x,y,width,height){
+        var gr = {
+            'isStatic':false,
+            'friction':0.4,
+            'density':1
+        }
+        this.body = Bodies.rectangle(x, y, width, height,gr);
+        this.width = width;
+        this.height = height;
+        this.x = x;
+        this.y = y;
+
+        //this.image = loadImage("images/1S.png");
+      World.add(world,this.body);
+    }
+    display(){
+        var posi = this.body.position;
+        rectMode(CENTER);
+       // fill("purple");
+       rect(posi.x,posi.y,this.width,this.height);
+      
+        
+    }
+};
---
+++ sketch.js
@@ -0,0 +1,54 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	
+	ground = new Ground(400,500,width,10);
+
+	paper = new Paper(100,100,20,20);
+	box = new Dustbin(595,200,15,100);
+	box2 = new Dustbin(650,300,100,15);
+	box3 = new Dustbin(705,200,15,100);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background("black");
+ 
+
+ 
+  paper.display();
+  box.display();
+  box2.display();
+  box3.display();
+  ground.display();
+  drawSprites();
+ 
+}
+keyPressed();
+function keyPressed(){
+	if (keyCode === UP_ARROW){
+		Matter.Body.applyForce(paper.body,paper.body.position,{x:0.04,y:-0.07});
+	}
+}
+
